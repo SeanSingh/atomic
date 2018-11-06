7df1b911e707604982fca5ce4a8d30034a1c1970
Sean Singh <sean@open.ngo>
2018-11-06 00:01:48 -0600

    Added systemd services

--- atomic_secondboot.sh
+++ atomic_secondboot.sh
@@ -5,4 +5,7 @@ net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
 
-sudo rpm-ostree pkg-add kubelet kubeadm kubectl kubernetes-cni
+echo "setting --add-registry registry.centos.org" | sudo tee -a /etc/sysconfig/docker
+sudo curl -o /etc/systemd/system/kube-apiserver.service -L https://raw.githubusercontent.com/SeanSingh/atomic/master/systemd/kube-apiserver.service
+sudo curl -o /etc/systemd/system/kube-controller-manager.service -L https://raw.githubusercontent.com/SeanSingh/atomic/master/systemd/kube-controller-manager.service
+sudo curl -o /etc/systemd/system/kube-scheduler.service -L https://raw.githubusercontent.com/SeanSingh/atomic/master/systemd/kube-scheduler.service
